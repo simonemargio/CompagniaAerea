66d0bb7c34606475574c63863b4bd230296e01ea
Simone Margio <simone.margio@icloud.com>
2020-06-07 15:01:52 +0200

    Add: aggiunti commenti Grafo.c e .h

--- Grafo.c
+++ Grafo.c
@@ -5,8 +5,22 @@
 #include "Error.h"
 
 
-
+/*
+ *  Descrizione: aggiunta di un nuovo nodo
+ *  Dettagli: aggiunge una nuova città nel grafo
+ *  Parametri in: G->struttura di gestione del grafo
+ *                nomeCitta->nome della città da inserire
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_aggiungi_destionazione_amministratore
+ *             CompagniaAerea->F_popoplamento_grafo_mappa_voli
+ *
+*/
 void F_aggiungi_nodo_grafo_lista(Grafo *G, char *nomeCitta){
+    /*
+     * Viene presa la struttura contenente la lista delle citta.
+     * L'inserimento di una nuova città viene fatto in coda.
+     * Si incrementa il numero dei nodi.
+     */
     ListaAdj L=(*G)->StrutturaGrafoPtr;
     F_alloca_nodo_grafo_lista(&L,nomeCitta);
 
@@ -14,7 +28,16 @@ void F_aggiungi_nodo_grafo_lista(Grafo *G, char *nomeCitta){
     (*G)->StrutturaGrafoPtr=L;
 }
 
-
+/*
+ *  Descrizione: alloca un nuovo nodo
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                nomeCitta->nome nuova città da inserire
+ *  Parametri out: //
+ *  Chiamante: Grafo->F_aggiungi_nodo_grafo_lista
+ *
+*/
 void F_alloca_nodo_grafo_lista(ListaAdj *L, char *nomeCitta){
     if(F_struttura_vuota(*L)) {
         (*L)=(struct struttura_grafo_lista_adiacenza_citta*)malloc(sizeof(struct struttura_grafo_lista_adiacenza_citta));
@@ -28,8 +51,23 @@ void F_alloca_nodo_grafo_lista(ListaAdj *L, char *nomeCitta){
     } else F_alloca_nodo_grafo_lista((&(*L)->nextPtr),nomeCitta);
 }
 
+/*
+ *  Descrizione: aggiunta di un arco
+ *  Dettagli: corrisponde all'aggiunta di un nuovo volo tra due città
+ *  Parametri in: G->struttura di gestione del grafo
+ *                cittaPartenza/arrivo: nome delle città in cui creare
+ *                un volo
+ *                tempo/costo: tempo e costo di volo
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_aggiungi_volo_amministratore
+ *             CompagniaAerea->F_popoplamento_grafo_mappa_voli
+ *
+*/
 void F_aggiungi_arco_grafo_lista(Grafo *G, char *cittaPartenza, char *cittaArrivo, float tempo, float costo){
     ListaAdj L=(*G)->StrutturaGrafoPtr;
+    /*
+     * Si verifica che le città di partenza e arrivano esistano nel grafo.
+     */
     ListaAdj nodoCittaPartenza=F_cerca_nodo_grafo_lista(&L,cittaPartenza);
 
     if(!F_struttura_vuota(nodoCittaPartenza)){
@@ -44,7 +82,25 @@ void F_aggiungi_arco_grafo_lista(Grafo *G, char *cittaPartenza, char *cittaArriv
     }else printf("La citta' (%s) di partenza non e' presente nella mappa.\n",cittaPartenza);
 }
 
+/*
+ *  Descrizione: esecuzione aggiunta di un arco
+ *  Dettagli: procedere ad inserire un arco tra due città
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                nomeCittaDaInserire: nome della città in cui arriva
+ *                l'arco
+ *                tempo/costo: tempo e costo di volo
+ *  Parametri out: //
+ *  Chiamante: Grafo->F_aggiungi_arco_grafo_lista
+ *
+*/
 void F_inserisci_arco_grafo_lista(ListaAdj *L, char *nomeCittaDaInserire, float tempo, float costo){
+    /*
+     * L'esistenza delle città di partenza e di arrivo non implica che si debba creare un arco.
+     * L'arco viene inserito in coda in maniera tale controllare se non è già presente un arco
+     * verso nomeCittaDaInserire.
+     *
+     */
     if(F_struttura_vuota(*L)) {
         F_alloca_nodo_grafo_lista(L,nomeCittaDaInserire);
         F_alloca_strutture_peso_tempo_e_costo(L,tempo,costo);
@@ -56,6 +112,14 @@ void F_inserisci_arco_grafo_lista(ListaAdj *L, char *nomeCittaDaInserire, float 
     }
 }
 
+/*
+ *  Descrizione: allocazione delle strutture peso e costo
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                tempo/costo: tempo e costo di volo
+ *
+*/
 void F_alloca_strutture_peso_tempo_e_costo(ListaAdj *L,float tempo, float costo){
     Peso pesoTempo=(struct struttura_grafo_pesato*)malloc(sizeof(struct struttura_grafo_pesato));
     Peso pesoCosto=(struct struttura_grafo_pesato*)malloc(sizeof(struct struttura_grafo_pesato));
@@ -66,6 +130,15 @@ void F_alloca_strutture_peso_tempo_e_costo(ListaAdj *L,float tempo, float costo)
     (*L)->pesoCostoPtr=pesoCosto;
 }
 
+/*
+ *  Descrizione: cerca di un nodo nel grafo
+ *  Dettagli: in base al nome della città viene ritornato il nodo se presente
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                cittaDaTrovare: nome città da trovare
+ *  Parametri out: L->nodo città trovato, NULL altrimenti
+ *
+*/
 ListaAdj F_cerca_nodo_grafo_lista(ListaAdj *L, char *cittaDaTrovare){
     if(F_struttura_vuota(*L)) return NULL;
     int cittaTrovata=F_confronto_stringhe(cittaDaTrovare,(*L)->nomeCittaPtr);
@@ -73,7 +146,16 @@ ListaAdj F_cerca_nodo_grafo_lista(ListaAdj *L, char *cittaDaTrovare){
     return F_cerca_nodo_grafo_lista((&(*L)->nextPtr),cittaDaTrovare);
 }
 
-
+/*
+ *  Descrizione: deallocazione di tutti archi di un nodo del grafo
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_elimina_destinazione_amministratore
+ *             Grafo->F_dealloca_struttura_grafo_lista
+ *
+*/
 void F_dealloca_archi_grafo_lista(ListaAdj *L){
     if (!F_struttura_vuota(*L)) {
         F_dealloca_archi_grafo_lista((&(*L)->arcoPtr));
@@ -82,6 +164,16 @@ void F_dealloca_archi_grafo_lista(ListaAdj *L){
     }
 }
 
+/*
+ *  Descrizione: deallocazione di un nodo del grafo
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                cittaDaEliminare->nome città da eliminare
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_elimina_destinazione_amministratore
+ *
+*/
 void F_dealloca_nodo_grafo_lista(ListaAdj *L, char *cittaDaEliminare){
     ListaAdj aux=(*L);
     if(*L){
@@ -93,6 +185,16 @@ void F_dealloca_nodo_grafo_lista(ListaAdj *L, char *cittaDaEliminare){
     }
 }
 
+/*
+ *  Descrizione: deallocazione di un solo arco tra due nodi
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *                cittaDaEliminare->nome città da eliminare
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_elimina_volo_amministratore
+ *
+*/
 void F_dealloca_arco_grafo_lista(ListaAdj *L, char *cittaDaEliminare){
     ListaAdj aux=(*L);
     if(*L){
@@ -104,6 +206,15 @@ void F_dealloca_arco_grafo_lista(ListaAdj *L, char *cittaDaEliminare){
     }
 }
 
+/*
+ *  Descrizione: deallocazione di tutto il grafo
+ *  Dettagli: //
+ *  Parametri in: L->lista contentente tutti i nodi e archi
+ *                del grafo
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_dealloca_strutture
+ *
+*/
 void F_dealloca_struttura_grafo_lista(ListaAdj *L){
     if(!F_struttura_vuota(*L)){
         F_dealloca_struttura_grafo_lista((&(*L)->nextPtr));
--- Grafo.h
+++ Grafo.h
@@ -1,13 +1,45 @@
 #ifndef COMPAGNIAAEREA_GRAFO_H
 #define COMPAGNIAAEREA_GRAFO_H
 
-
+/*
+ * Descrizione: struttura principale per la gestione del grafo
+ * Dettagli: gestisce tutte le informazioni che ruotano attorno alla struttura
+ *           delle città
+ * Contenuto: StrutturaGrafoPtr: punta al nodo iniziale della struttura delle città
+ *            numeroNodi: numero totale di città presenti
+ */
 struct struttura_gestione_grafi{
     struct struttura_grafo_lista_adiacenza_citta *StrutturaGrafoPtr;
     int numeroNodi;
 };
 
+/*
+ * Descrizione: identifica un nodo nel grafo
+ * Dettagli: nodi (città nuove) e archi (collegamenti tra due nodi) vengono sempre
+ *           espressi mediante l'uso di tale struttura.
+ * Contenuto: pesoTempoPtr/pesoCostoPtr: se si sta creando un arco tra due nodi allora
+ *            vengono allocate le strutture per i pesi e collegate alla struttura
+ *            nextPtr: permette la navigazione tra le città nel grafo
+ *            arcoPtr: permette la navigazione tra i gli archi di una specifica città
+ *            nomeCittaPtr: nome della città
+ *            visite: valore numerico incrementato ogni volta che un utente acquista un
+ *            biglietto aereo che ha come destinazione una delle città presenti
+ */
 struct struttura_grafo_lista_adiacenza_citta{
+    /*
+     * La reppresentazione del grafo tramite liste di permette di usufruire di un'unica
+     * struttura per poter gestire sia nodi che archi.
+     * Se bisogna allocare solo un nodo allora pesoTempoPtr/pesoCostoPtr/arcoPtr restano a NULL.
+     * Se bisogna allorare degli archi nel nodo di riferimento allora si creano le strutture
+     * apposite per i pesi e si collega l'arco tramite arcoPtr.
+     *
+     * In termini semplici:
+     * Napoli->Roma->Ibiza
+     * Firenze
+     *
+     * Per Navigare da Napoli a Firenze uso nextPtr.
+     * Per navigare da Napoli a Ibiza uso arcoPtr.
+     */
     char *nomeCittaPtr;
     int visite;
 
@@ -17,6 +49,11 @@ struct struttura_grafo_lista_adiacenza_citta{
     struct struttura_grafo_lista_adiacenza_citta *nextPtr; /*  Puntatore al nodo successivo */
 };
 
+/*
+ * Descrizione: struttura del peso associato ad un arco
+ * Dettagli: allocata sia per il peso del tempo che del costo di un volo
+ * Contenuto: peso: peso tempo o costo
+ */
 struct struttura_grafo_pesato{
     float peso;
 };
