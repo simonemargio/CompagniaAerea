a5ca01a51c8b7e7a14f4c88ada1ac69b2d404c27
Simone Margio <simone.margio@icloud.com>
2020-06-07 15:29:12 +0200

    Add: aggiunti commenti Abr.c e .h

--- Abr.c
+++ Abr.c
@@ -3,7 +3,22 @@
 #include "Abr.h"
 #include "CompagniaAerea.h"
 #include "Error.h"
+/*
+ * In termini generali tutte le operazioni sugli alberi sfruttano come
+ * indice l'username (che sia amministratore o utente) per spostarsi a
+ * destra o sinistra dell'albero stesso.
+ *
+ */
 
+/*
+ *  Descrizione: inserimento di un amministratore
+ *  Dettagli: esegue l'inserimento di un amministratore nell'albero
+ *  Parametri in: T->albero amministratori
+ *                amministratoreDaInserire->struttura del nuovo amministratore
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_polamento_automatico_amministratori
+ *
+ */
 void F_inserisci_amministratore_abr(AlberoAmministratore *T, Amministratore amministratoreDaInserire){
     if(F_struttura_vuota(*T)){
         F_alloca_struttura_amministratore_abr(T);
@@ -17,7 +32,14 @@ void F_inserisci_amministratore_abr(AlberoAmministratore *T, Amministratore ammi
     }
 }
 
-
+/*
+ *  Descrizione: allocazione di un nuovo nodo amministratore
+ *  Dettagli: //
+ *  Parametri in: nuovoAmministratore->nodo da allocare
+ *  Parametri out: //
+ *  Chiamante: Abr->F_inserisci_amministratore_abr
+ *
+*/
 void F_alloca_struttura_amministratore_abr(AlberoAmministratore *nuovoAmministratore){
     (*nuovoAmministratore)=(struct struttura_nodo_albero_amministratore*)malloc(sizeof(struct struttura_nodo_albero_amministratore));
     if(F_struttura_vuota(*nuovoAmministratore)) F_error(3);
@@ -26,7 +48,15 @@ void F_alloca_struttura_amministratore_abr(AlberoAmministratore *nuovoAmministra
     (*nuovoAmministratore)->dxPtr=NULL;
 }
 
-
+/*
+ *  Descrizione: inserimento di un utente
+ *  Dettagli: esegue l'inserimento di un utente nell'albero
+ *  Parametri in: T->albero utenti
+ *                utenteDaInserire->struttura del nuovo utente
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_registrazione_utente
+ *
+*/
 void F_inserisci_utente_abr(AlberoUtente *T, Utente utenteDaInserire){
     if(F_struttura_vuota(*T)){
         F_alloca_struttura_utente_abr(T);
@@ -40,7 +70,14 @@ void F_inserisci_utente_abr(AlberoUtente *T, Utente utenteDaInserire){
     }
 }
 
-
+/*
+ *  Descrizione: allocazione di un nuovo nodo utente
+ *  Dettagli: //
+ *  Parametri in: nuovoUtente->nodo da allocare
+ *  Parametri out: //
+ *  Chiamante: Abr->F_inserisci_utente_abr
+ *
+*/
 void F_alloca_struttura_utente_abr(AlberoUtente *nuovoUtente){
     (*nuovoUtente)=(struct struttura_nodo_albero_utente*)malloc(sizeof(struct struttura_nodo_albero_utente));
     if(F_struttura_vuota(*nuovoUtente)) F_error(5);
@@ -49,6 +86,16 @@ void F_alloca_struttura_utente_abr(AlberoUtente *nuovoUtente){
     (*nuovoUtente)->dxPtr=NULL;
 }
 
+/*
+ *  Descrizione: cerca un amministratore nell'albero
+ *  Dettagli:  //
+ *  Parametri in: T->albero amministratori
+ *                amministratoreDaCercare->username dell'amministratore
+ *  Parametri out: amministratoreAlbero->nodo contentente l'amministratore trovato
+ *                 NULL->altrimenti
+ *  Chiamante: CompagniaAerea->F_registrazione_utente
+ *
+*/
 Amministratore F_cerca_amministratore_abr(AlberoAmministratore *T, char *amministratoreDaCercare){
     if(F_struttura_vuota(*T)) return NULL;
     else{
@@ -63,6 +110,16 @@ Amministratore F_cerca_amministratore_abr(AlberoAmministratore *T, char *amminis
     }
 }
 
+/*
+ *  Descrizione: cerca un utente nell'albero
+ *  Dettagli:  //
+ *  Parametri in: T->albero utenti
+ *                utenteDaCercare->username dell'utente
+ *  Parametri out: utenteAlbero->nodo contentente l'utente trovato
+ *                 NULL->altrimenti
+ *  Chiamante: CompagniaAerea->F_registrazione_utente
+ *
+*/
 Utente F_cerca_utente_abr(AlberoUtente *T, char *utenteDaCercare){
     if(F_struttura_vuota(*T)) return NULL;
     else{
@@ -77,6 +134,14 @@ Utente F_cerca_utente_abr(AlberoUtente *T, char *utenteDaCercare){
     }
 }
 
+/*
+ *  Descrizione: deallocazione di tutto l'albero degli amministratori
+ *  Dettagli: //
+ *  Parametri in: T->albero amminsitratori
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_dealloca_strutture
+ *
+*/
 void F_dealloca_struttura_albero_amministratore(AlberoAmministratore *T){
     if(!F_struttura_vuota(*T)){
         F_dealloca_struttura_albero_amministratore((&(*T)->sxPtr));
@@ -87,6 +152,14 @@ void F_dealloca_struttura_albero_amministratore(AlberoAmministratore *T){
     }
 }
 
+/*
+ *  Descrizione: deallocazione di tutto l'albero degli utenti
+ *  Dettagli: //
+ *  Parametri in: T->albero utenti
+ *  Parametri out: //
+ *  Chiamante: CompagniaAerea->F_dealloca_strutture
+ *
+*/
 void F_dealloca_struttura_albero_utente(AlberoUtente *T){
     if(!F_struttura_vuota(*T)){
         F_dealloca_struttura_albero_utente((&(*T)->sxPtr));
